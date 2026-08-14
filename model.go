1c7d117d2df4dbb7063eaf710b164bc644da3afe
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-08-13 23:48:33 -0700

    feat: add Update method with quit-on-q handling

--- model.go
+++ model.go
@@ -1,8 +1,14 @@
 package main
 
+import tea "github.com/charmbracelet/bubbletea"
+
 type Model struct {
 	Width      int
 	Height     int
 	ActivePane int
 	WillQuit   bool
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
 }
