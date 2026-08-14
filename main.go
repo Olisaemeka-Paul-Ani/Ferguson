d4da866c424578f41bd02d983c8b9afe0b7637b0
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-08-14 01:10:48 -0700

    no config file in V1, use --team flag

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	nFlag := flag.Int("team", 0, "This is supposed to be your FPL team ID")
+	flag.Parse()
+	fmt.Println(*nFlag)
+	m := Model{}
+	p := tea.NewProgram(m)
+	_, err := p.Run()
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+}
