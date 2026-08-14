0f69422b4c3aec7f6390f2adb40bcf8cf150e9cb
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-08-13 20:49:29 -0700

    feat: add Player struct with FPL API json tags

--- fpl/types.go
+++ fpl/types.go
@@ -1,1 +1,11 @@
 package fpl
+
+type Player struct {
+	WebName     string `json:"web_name"`
+	Position    int    `json:"element_type"`
+	Club        int    `json:"team"`
+	Cost        int    `json:"now_cost"`
+	TotalPoints int    `json:"total_points"`
+
+	GameweekPoints int `json:"event_points"`
+}
