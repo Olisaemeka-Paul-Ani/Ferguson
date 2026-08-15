e14f1636f515dc65478c0ada7c59fdf5a3efe47e
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-08-14 17:23:43 -0700

    feat: implement FetchAllPlayers with FPL API HTTP client and JSON unmarshaling

--- fpl/client.go
+++ fpl/client.go
@@ -1,1 +1,33 @@
 package fpl
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type Container struct {
+	Group []Player `json:"elements"`
+}
+
+func FetchAllPlayers() ([]Player, error) {
+	resp, err := http.Get("https://fantasy.premierleague.com/api/bootstrap-static/")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+	var PlayerList Container
+	err = json.Unmarshal(body, &PlayerList)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return PlayerList.Group, nil
+}
