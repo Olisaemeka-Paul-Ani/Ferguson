fcb6f1158f690af07bb76a27fef8692b8b54358f
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-08-16 22:27:54 -0700

    feat: add FindUpcomingMatches and FirstFive helpers to ui/fixtures.go

--- ui/fixtures.go
+++ ui/fixtures.go
@@ -1,1 +1,36 @@
 package ui
+
+import (
+	"github.com/Olisaemeka-Paul-Ani/ferguson/fpl"
+)
+
+func FindUpcomingMatches(games []fpl.Fixture) []fpl.Fixture {
+	var output []fpl.Fixture
+	i := 0
+	for i < len(games) {
+		if !(games[i].Finished) {
+			output = append(output, games[i])
+		}
+		i = i + 1
+
+	}
+	return output
+}
+
+func FirstFive(games []fpl.Fixture) []fpl.Fixture {
+	var output []fpl.Fixture
+	i := 0
+	if len(games) < 5 {
+		for i < len(games) {
+			output = append(output, games[i])
+			i = i + 1
+		}
+		return output
+	}
+
+	for i < 5 {
+		output = append(output, games[i])
+		i = i + 1
+	}
+	return output
+}
