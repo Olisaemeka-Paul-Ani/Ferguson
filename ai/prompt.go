a76a65d89ed4ca62fcc2d6803bd2f5a9b79fd808
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-08-24 23:52:38 -0700

    feat: add BundlePlayerData to append formatted squad data to prompt string

--- ai/prompt.go
+++ ai/prompt.go
@@ -1,1 +1,21 @@
 package ai
+
+import (
+	"github.com/Olisaemeka-Paul-Ani/ferguson/fpl"
+	"github.com/Olisaemeka-Paul-Ani/ferguson/ui"
+)
+
+func BundlePlayerData(strInput string) (string, error) {
+	test, err := fpl.FetchAllPlayers()
+	if err != nil {
+		return "", err
+	}
+
+	str := ui.CleanData(test)
+	output := ui.FormatData(str)
+
+	strInput = strInput + output
+
+	return strInput, nil
+
+}
