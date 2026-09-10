b8b4502cdaa09f78eeed74c569d42d6fb3690094
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-09-10 16:25:37 -0700

    feat: add Directions content and keybind map for help overlay

--- ui/help.go
+++ ui/help.go
@@ -1,1 +1,19 @@
 package ui
+
+var Directions string = "DIRECTIONS"
+
+var DirectionsCommands = map[string]string{
+	"ENTER":      "View specific Player Stats",
+	"ESC":        "Exit specific player stats",
+	"UP/DOWN":    "Toggle layer selection/View depending on if enter command was pressed",
+	"RIGHT/LEFT": "Toggle Active Pane",
+	"?":          "Toggle Help/About Page",
+}
+
+var DirectionsCommandsKeys = []string{
+	"?",
+	"ENTER",
+	"ESC",
+	"RIGHT/LEFT",
+	"UP/DOWN",
+}
