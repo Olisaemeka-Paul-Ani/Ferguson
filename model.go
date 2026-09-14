b3a4e87fd78bef56aab909f0d267ec0289957ff8
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-09-13 20:16:08 -0700

    feat: capture real terminal size via tea.WindowSizeMsg

--- model.go
+++ model.go
@@ -219,6 +219,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		}
 
+	case tea.WindowSizeMsg:
+		m.Width = msg.Width
+		m.Height = msg.Height
+
 	case FormSheet:
 		if msg.Err != nil {
 			m.FormErr = msg.Err
